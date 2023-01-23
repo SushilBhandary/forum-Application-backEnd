57f8d5f0515378548526793eadcd715f821592d0
Sushil <102742394+SushilBhandary@users.noreply.github.com>
2023-01-23 21:30:57 +0530

    middleware

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,22 @@
+const JWT = require("jsonwebtoken")
+const config = require("../config/config")
+
+exports.auth = (req, res, next) => {
+    const token = req.headers.authorization
+    if (!token) {
+        return res.status(401).json({
+            error : "Not authorized to access this route 1"
+        })
+    }
+    try {
+        const decode = JWT.verify(token, config.JWT_SECRET)
+        req.user = decode
+       
+    } catch (error) {
+        return res.status(401).json({
+            error : "Not authorized to access this routec 2"
+        })
+    }
+    return next()
+}
+
