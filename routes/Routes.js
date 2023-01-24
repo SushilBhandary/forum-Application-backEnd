5ee13756b09e5293aeecf965743be453280b8c49
Sushil <102742394+SushilBhandary@users.noreply.github.com>
2023-01-24 16:57:02 +0530

    updating routers

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const {auth} = require("../middlewares/auth.middlewares")
+const  {
+    signUp,
+    login,
+    logout
+} = require('../controllers/auth.controller')
+const  {
+    getPosts,
+    createPost,
+    editPost,
+    deletePost,
+    addComment
+} = require('../controllers/post.controller')
 
 router.get("/", (req, res) => {
     res.send("Hello auth system")
 })
-router.post("/signup");
-router.post("/login");
-router.post("/logout");
-router.post("/get-post");
-router.post("/create-post");
-router.put("/edit-post");
-router.delete("/delete-post");
-router.post("/add-comments");
+router.post("/signup",signUp);
+router.post("/login", login);
+router.post("/logout", auth, logout);
+router.post("/get-post", auth, getPosts);
+router.post("/create-post/:userid", auth, createPost);
+router.post("/create-post/:userid", auth, createPost);
+router.put("/edit-post/:postid/:userid", auth, editPost);
+router.delete("/delete-post/:postid/:userid", auth, deletePost);
+router.post("/add-comments/:postid", auth, addComment);
+
 module.exports = router;
